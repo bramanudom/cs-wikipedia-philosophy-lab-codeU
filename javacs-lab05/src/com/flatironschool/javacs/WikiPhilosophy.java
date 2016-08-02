7522e07a38202c1c93e0545ee52957e4bccdb7dd
bramanudom <bramanud@wellesley.edu>
2016-08-01 17:10:50 -0700

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -13,6 +13,10 @@ import org.jsoup.select.Elements;
 public class WikiPhilosophy {
 	
 	final static WikiFetcher wf = new WikiFetcher();
+	static	ArrayList urlsVisited = new ArrayList();
+	final static String finalUrl = "https://en.wikipedia.org/wiki/Philosophy";
+	static boolean finishedCrawling = false;
+
 	
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
@@ -27,25 +31,113 @@ public class WikiPhilosophy {
 	 * @param args
 	 * @throws IOException
 	 */
+
 	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
 
-		Element firstPara = paragraphs.get(0);
+		WikiPhilosophy test = new WikiPhilosophy();
+		test.crawl("https://en.wikipedia.org/wiki/Java_(programming_language)");
 		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
+
+	}
+
+	/* crawl: provides actual functionality
+		@params: a String that represents the url at which to start crawling from
+		@return: void, but will print out a list of urls visited in the console
+	*/
+
+	public static void crawl (String url){
+		String currentLink = url;
+
+		while(!finishedCrawling){
+
+			try{
+
+				if(findFirstLink(currentLink).equals(finalUrl)){
+					urlsVisited.add(currentLink);
+					finishedCrawling = true;
+				}
+
+			urlsVisited.add(currentLink);
+			//System.out.println(currentLink);
+			currentLink = findFirstLink(currentLink);
+			//System.out.println(currentLink);
 			}
-        }
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+			catch (IOException e){
+				System.out.print("There was an error crawling");
+
+			}
+
+			catch (Exception e){
+				System.out.print(e);
+			}
+	
+		}
+
+		System.out.print(urlsVisited);	
+
+	}
+
+	/* findFirstLink: helper method
+		@params: takes in a String that represents the URL of the page to search through
+		@return: the String representation of the first link found on the page 
+		Throws an exception if there are issues crawling or if the page has 
+		no (valid) links
+	*/
+	public static String findFirstLink (String url) throws IOException, Exception{
+		Elements doc = wf.fetchWikipedia(url);
+		Element  firstpara = doc.get(0);
+		Iterable <Node> iter = new WikiNodeIterable(firstpara);
+		int numRParen = 0;
+		int numLParen = 0;
+		boolean foundParen = false;
+		for (Node node: iter) {
+
+			/*	if the node encountered is a textnode, we want to keep track of the 
+				parenthesis - - all non textNodes that are encountered should be ignored 
+				until all parenthesis are closed. 
+			*/
+
+			if (node instanceof TextNode){
+				TextNode accesibleNode = (TextNode)node;
+				char[] chars = accesibleNode.text().toCharArray();
+					for (char c: chars){
+						if (c == '('){
+							// System.out.println("found a paren");
+							// System.out.println(node);
+							numRParen ++;
+						}
+						else if (c == ')') {
+							numLParen++;
+						}
+					}
+					
+				}
+			else if ((numLParen == numRParen) && node instanceof Element){
+				Element accesibleNode = (Element)node;
+				String tag = accesibleNode.tagName();
+					if(tag.equals("a") && isValidLink(accesibleNode)){
+					return accesibleNode.attr("abs:href");
+					}
+			}
+		}
+		throw new Exception("No links found");
+	}
+
+		
+
+
+	/* isValidLink: helper method; takes in a Element object and an iterator
+		checks to see if there aare i or em tages up the parent tree
+		returns a boolean indicating whether or not the link is valid
+	*/
+
+
+	public static boolean isValidLink(Element link){
+		Element parent = link.parent();
+		String parentTag = parent.tagName();
+		return !(parentTag.equals("i") || parentTag.equals("em"));
+	
 	}
 }
